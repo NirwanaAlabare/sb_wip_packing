8a2bf48a6f7752d7125d4a4166ba11980202577e
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-03-10 16:14:45 +0700

    change to finish

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="icon" type="image/x-icon" href="{{ asset('images/packingline.ico') }}">
-    <title>NDS PACKING LINE</title>
+    <title>NDS FINISH LINE</title>
 
     @include('layouts.link')
 
@@ -20,12 +20,12 @@
             <div class="col-md-6">
                 <div class="d-flex flex-column justify-content-center align-items-center">
                     <img src="{{ asset('images/Frame 1.png') }}" class="img-fluid mt-auto mb-auto" alt="...">
-                    <p class="fs-5 mt-3 w-75 mb-0 bg-success text-light text-center rounded-3">PACKING LINE</p>
+                    <p class="fs-5 mt-3 w-75 mb-0 bg-sb-secondary text-light text-center rounded-3">PACKING LINE</p>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="card-body my-5">
-                    <h2 class="text-center text-success fw-bold mb-3">LOGIN</h2>
+                    <h2 class="text-center text-sb-secondary fw-bold mb-3">LOGIN</h2>
                     <form method="POST" action="{{ url('login/authenticate') }}" onsubmit="login(this, event)"
                         class="login-form mx-3">
                         @csrf
@@ -53,7 +53,7 @@
                             @enderror
                         </div>
                         <div class="mt-3 mb-3">
-                            <button type="submit" class="btn btn-success w-100 mt-3 mb-3">LOGIN</button>
+                            <button type="submit" class="btn btn-sb-secondary w-100 mt-3 mb-3">LOGIN</button>
                         </div>
                     </form>
                 </div>
